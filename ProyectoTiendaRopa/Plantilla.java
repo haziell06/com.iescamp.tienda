847dcfab5c6b219552cdd2446e58713490e35122
Christian <chrisgs2020@gmail.com>
2025-03-01 18:51:48 +0100

    Add files via upload

---
+++ ProyectoTiendaRopa/Plantilla.java
@@ -0,0 +1,62 @@
+package com.iescamp.tienda;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Plantilla {
+    private ArrayList<Empleado> plantillas = new ArrayList<>();
+
+    public Plantilla(ArrayList<Empleado> plantillas) {
+        this.plantillas = plantillas;
+    }
+
+    public ArrayList<Empleado> getPlantillas() {
+        return plantillas;
+    }
+
+    public void setPlantillas(ArrayList<Empleado> plantillas) {
+        this.plantillas = plantillas;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(plantillas);
+    }
+
+    public Empleado buscarDNI(String DNI) {
+        for (Empleado empleado : plantillas) {
+            if (empleado.getDNI().equals(DNI)) {
+                return empleado;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Empleado> filtarDep(String Dep) {
+        ArrayList<Empleado> DepBuscado = new ArrayList<>();
+        for (Empleado empleado : plantillas) {
+            if (empleado.getDepartamento().equals(Dep)) {
+                DepBuscado.add(empleado);
+            }
+        }
+        return DepBuscado;
+    }
+    public ArrayList<Empleado> filtarAdmin() {
+        ArrayList<Empleado> Administradores = new ArrayList<>();
+        for (Empleado empleado : plantillas) {
+            if (empleado.isTienePrivilegios()) {
+                Administradores.add(empleado);
+            }
+        }
+        return Administradores;
+    }
+    public ArrayList<Empleado> filtarNom(String Nom, String Apellidos) {
+        ArrayList<Empleado> NomBuscado = new ArrayList<>();
+        for (Empleado empleado : plantillas) {
+            if (empleado.getNombre().equals(Nom) && (empleado.getApellidos().equals(Apellidos))) {
+                NomBuscado.add(empleado);
+            }
+        }
+        return NomBuscado;
+    }
+}
