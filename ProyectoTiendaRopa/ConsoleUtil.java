18c1cfa7fe074ef81079ecc1a0853a27bf81f332
Christian <chrisgs2020@gmail.com>
2025-03-03 18:58:05 +0100

    Add files via upload

--- ProyectoTiendaRopa/ConsoleUtil.java
+++ ProyectoTiendaRopa/ConsoleUtil.java
@@ -32,23 +32,38 @@ public class ConsoleUtil {
         String descripcion = readString("Descripción: ");
         String imagen = readString("Imagen: ");
         boolean activo = readBoolean("¿Es activo? ");
-        String tallaRopa = readString("Talla: ");
+        int talla = readInt("Talla: ");
         String tipoCierre = readString("Tipo cierre: ");
         boolean impermeable = readBoolean("¿Es impermeable? ");
 
-        return new Chaqueta(cod_art, nombre, precio, marca, descripcion, imagen, activo, tallaRopa, tipoCierre, impermeable);
+        return new Chaqueta(cod_art, nombre, precio, marca, descripcion, imagen, activo, talla, tipoCierre, impermeable);
     }
 
-        public static Bolso crearBolso(){
+    public static Bolso crearBolso() {
         System.out.println("Creando un Bolso...");
         String estilo = readString("Estilo: ");
         String esPersonalizado = readString("¿Es personalizado? (Sí/No): ");
         String tipoCierre = readString("Tipo de cierre: ");
         int capacidad = readInt("Capacidad en litros: ");
-        
+
         return new Bolso(estilo, esPersonalizado, tipoCierre, capacidad);
+    }
 
+    public static Camisa crearCamisa(Material material) {
+        System.out.println("Creando una Camisa...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        String marca = readString("Marca: ");
+        int talla = readInt("Talla: ");
+        String color = readString("Color: ");
+        String tipoCierre = readString("Tipo cierre: ");
+        String tipoManga = readString("Tipo Manga:  ");
+        boolean esEstampada = readBoolean("¿Tiene Estampado? ");
 
-
-    
+        return new Camisa(cod_art, nombre, precio, descripcion, imagen, activo, marca, talla, color, tipoCierre, tipoManga, esEstampada);
+    }
 }
