0c796ec4c21248795fdf03c6a45c2ad240615b57
Christian <chrisgs2020@gmail.com>
2025-03-12 13:57:29 +0100

    Add files via upload

---
+++ ProyectoTiendaRopa/Catalogo.java
@@ -0,0 +1,46 @@
+package com.iescamp.tienda;
+
+import java.util.ArrayList;
+
+public class Catalogo {
+    private ArrayList<Articulo> catalogo = new ArrayList<>();
+
+    public Catalogo(ArrayList<Articulo> catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public ArrayList<Articulo> getCatalogo() {
+        return catalogo;
+    }
+
+    public void setCatalogo(ArrayList<Articulo> catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public Articulo buscarDni(int Cod) {
+        for (Articulo articulo : catalogo) {
+            if (articulo.getCodigoArticulo() == Cod) {
+                return articulo;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Articulo> filtarActivo() {
+        ArrayList<Articulo> filtroActivo = new ArrayList<>();
+        for (Articulo articulo : catalogo) {
+            if (articulo.isActivo()) {
+                filtroActivo.add(articulo);
+            }
+        }
+        return filtroActivo;
+    }
+    public ArrayList<Articulo> filtarMaterial(String material) {
+        ArrayList<Articulo> filtroMaterial = new ArrayList<>();
+        for (Articulo articulo : catalogo) {
+            if (articulo.getMaterial().equals(material)) {
+                filtroMaterial.add(articulo);
+            }
+        }
+        return filtroMaterial;
+    }
+}
