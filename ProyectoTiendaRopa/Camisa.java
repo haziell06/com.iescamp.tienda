578c8723099ba859d8cf26fc19baa68ad7666e1e
Haziel <haziel030306@gmail.com>
2025-03-12 13:28:32 +0100

    Update Camisa.java

--- ProyectoTiendaRopa/Camisa.java
+++ ProyectoTiendaRopa/Camisa.java
@@ -6,13 +6,13 @@ public class Camisa extends Ropa {
     private String tipoManga;
     private boolean esEstampada;
 
-    public Camisa(int talla, String color, String tipoCierre, String tipoManga, boolean esEstampada) {
+    public Camisa(String talla, String color, String tipoCierre, String tipoManga, boolean esEstampada) {
         super(talla, color, tipoCierre);
         this.tipoManga = tipoManga;
         this.esEstampada = esEstampada;
     }
 
-    public Camisa(int codArt, String nombre, BigDecimal precio, String descripcion, String imagen, boolean activo, String marca, int talla, String color, String tipoCierre, String tipoManga, boolean esEstampada) {
+    public Camisa(int codArt, String nombre, BigDecimal precio, String descripcion, String imagen, boolean activo, String marca, String talla, String color, String tipoCierre, String tipoManga, boolean esEstampada) {
         super(talla, color, tipoCierre);
     }
 
