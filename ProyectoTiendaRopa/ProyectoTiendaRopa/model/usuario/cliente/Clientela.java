37bb3db834590de1e63b6b539994fc4918d75c79
SEBAS <juavalmar2@alu.edu.gva.es>
2025-03-26 10:56:29 +0100

    Add files via upload

---
+++ ProyectoTiendaRopa/ProyectoTiendaRopa/model/usuario/cliente/Clientela.java
@@ -0,0 +1,66 @@
+package com.iescamp.tienda.model.usuario.cliente;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Clientela implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private List<Cliente> clientes;
+
+    // constructor
+    public Clientela(){
+        this.clientes = new ArrayList<>();
+    }
+
+
+    // getters y setters
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    // Buscar por DNI
+    public Cliente buscarDNI(String dni){
+        for (Cliente cliente : clientes){
+            if (cliente.getDni().equalsIgnoreCase(dni))
+                return cliente;
+        }
+        return null; //esto será cuando no se encuentre al cliente
+    }
+
+    // Filtrar por metodo de pago
+    public List<Cliente> filtrarPorMetodoPago(MetodoPago metodoPago) {
+        List<Cliente> resultado = new ArrayList<>();
+        for (Cliente cliente : clientes) {
+            if (cliente.getMetodoPago() == metodoPago) {
+                resultado.add(cliente);
+            }
+        }
+        return resultado;
+    }
+
+    // Filtrar por nombre y apellidos
+    public List<Cliente> filtrarPorNombreYApellidos(String nombre, String apellidos) {
+        List<Cliente> resultado = new ArrayList<>();
+        for (Cliente cliente : clientes) {
+            if (cliente.getNombre().equalsIgnoreCase(nombre) &&
+                    cliente.getApellidos().equalsIgnoreCase(apellidos)) {
+                resultado.add(cliente);
+            }
+        }
+        return resultado;
+    }
+
+    // metodo para añadir cliente
+    public void agregarCliente(Cliente cliente) {
+        if (cliente != null) {
+            clientes.add(cliente);
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
---
+++ ProyectoTiendaRopa/ProyectoTiendaRopa/uitl/ConsoleUtil.java
@@ -0,0 +1,93 @@
+package com.iescamp.tienda;
+
+import java.math.BigDecimal;
+
+import static com.iescamp.tienda.ConsoleReader.*;
+
+public class ConsoleUtil {
+    public static Zapato crearZapato(Material material) {
+        System.out.println("Creando un Zapato...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String marca = readString("Marca: ");
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        String color = readString("Color: ");
+        String estilo = readString("Estilo: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        boolean personalizado = readBoolean("¿Es personalizado? ");
+        String tipoSuela = readString("Tipo de suela: ");
+        int tallaZapato = readInt("Talla del zapato: ");
+
+        return new Zapato(cod_art, nombre, precio, marca, descripcion, true, imagen, color, material, estilo, activo, personalizado, TipoAccesorio.ZAPATOS, tipoSuela, tallaZapato);
+    }
+
+    public static Chaqueta crearChaqueta(Material material){
+        System.out.println("Creando una Chaqueta...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String marca = readString("Marca: ");
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        String talla = readString("Talla: ");
+        String tipoCierre = readString("Tipo cierre: ");
+        boolean impermeable = readBoolean("¿Es impermeable? ");
+
+        return new Chaqueta(talla, nombre, precio, marca, descripcion, imagen, activo, cod_art, tipoCierre, impermeable);
+    }
+
+    public static Bolso crearBolso(Material material) {
+        System.out.println("Creando un Bolso...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String marca = readString("Marca: ");
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        String estilo = readString("Estilo: ");
+        boolean personalizado = readBoolean("¿Es personalizado? ");
+        String tipoCierre = readString("Tipo de cierre: ");
+        int capacidad = readInt("Capacidad en litros: ");
+
+        return new Bolso(cod_art, nombre, precio, marca, descripcion, imagen, activo, estilo, personalizado, tipoCierre, capacidad);
+    }
+
+    public static Camisa crearCamisa(Material material) {
+        System.out.println("Creando una Camisa...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        String marca = readString("Marca: ");
+        String talla = readString("Talla: ");
+        String color = readString("Color: ");
+        String tipoCierre = readString("Tipo cierre: ");
+        String tipoManga = readString("Tipo Manga:  ");
+        boolean esEstampada = readBoolean("¿Tiene Estampado? ");
+
+        return new Camisa(cod_art, nombre, precio, descripcion, imagen, activo, marca, talla, color, tipoCierre, tipoManga, esEstampada);
+    }
+    public static Pantalon crearPantalon(Material material) {
+        System.out.println("Creando un Pantalon...");
+        int cod_art = readInt("Código del artículo: ");
+        String nombre = readString("Nombre: ");
+        BigDecimal precio = BigDecimal.valueOf(readDouble("Precio: "));
+        String descripcion = readString("Descripción: ");
+        String imagen = readString("Imagen: ");
+        boolean activo = readBoolean("¿Es activo? ");
+        String marca = readString("Marca: ");
+        String talla = readString("Talla: ");
+        String color = readString("Color: ");
+        String tipoCierre = readString("Tipo cierre: ");
+        String tipoPantalon = readString("Tipo Pantalon:  ");
+        boolean tieneBolsillos = readBoolean("¿Tiene Bolsillos? ");
+
+        return new Pantalon(cod_art, nombre, precio, descripcion, imagen, activo, marca, talla, color, tipoCierre, tipoPantalon, tieneBolsillos);
+    }
+}
