36e4db6c1660847a8f7503ebc5c4048c08653da6
Christian <chrisgs2020@gmail.com>
2025-03-27 19:15:54 +0100

    Update Chaqueta.java

--- ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
+++ ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
@@ -25,6 +25,11 @@ public class Chaqueta extends Ropa {
         this.impermeable = impermeable;
     }
 
+    public Chaqueta(int codArt, String nombre, BigDecimal precio, String descripcion, String imagen, boolean activo, String marca, String color, Material material, String talla, String tipoCierre, boolean impermeable) {
+        super(talla, tipoCierre);
+        this.impermeable = impermeable;
+    }
+
     public boolean isImpermeable() {
         return impermeable;
     }
