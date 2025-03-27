a6026841616da12cfcf85d58a46e18f181b693ca
PabloGamer79 <pablogimenezhortelano@gmail.com>
2025-03-27 17:30:40 +0100

    Update Chaqueta.java

--- ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
+++ ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
@@ -1,15 +1,26 @@
 package com.iescamp.tienda.model.articulo.ropa;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // Evita errores con campos desconocidos
 public class Chaqueta extends Ropa {
     private boolean impermeable;
 
-    public Chaqueta(String talla, String color, BigDecimal precio, String tipoCierre, String descripcion, String imagen, String s, String estilo, boolean impermeable, String string, String cierre, boolean b) {
-        super(talla, color, tipoCierre);
-        this.impermeable = impermeable;
+    // Constructor vacío para Jackson
+    public Chaqueta() {
+        super();
     }
 
-    public Chaqueta(String talla, String color, BigDecimal precio, String tipoCierre, String descripcion, String imagen, boolean impermeable, int i, String cierre, boolean b) {
+    public Chaqueta(
+            @JsonProperty("talla") String talla,
+            @JsonProperty("color") String color,
+            @JsonProperty("precio") BigDecimal precio,
+            @JsonProperty("tipoCierre") String tipoCierre,
+            @JsonProperty("descripcion") String descripcion,
+            @JsonProperty("imagen") String imagen,
+            @JsonProperty("impermeable") boolean impermeable) {
         super(talla, color, tipoCierre);
         this.impermeable = impermeable;
     }
