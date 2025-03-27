71403a8e262b20f755db6cebaf5f5900064689a9
PabloGamer79 <pablogimenezhortelano@gmail.com>
2025-03-27 17:22:45 +0100

    Update Bolso.java

--- ProyectoTiendaRopa/model/articulo/accesorio/Bolso.java
+++ ProyectoTiendaRopa/model/articulo/accesorio/Bolso.java
@@ -1,17 +1,31 @@
 package com.iescamp.tienda.model.articulo.accesorio;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Bolso extends Accesorio {
+    @JsonProperty("cierre")
     private String tipoCierre;
+
+    @JsonProperty("capacidad")
     private int capacidad;
 
-    public Bolso(String estilo, Boolean esPersonalizado, String tipoCierre, int capacidad) {
+    // Constructor vacío para deserialización
+    public Bolso() {
+    }
+
+    public Bolso(
+            @JsonProperty("estilo") String estilo,
+            @JsonProperty("personalizado") Boolean esPersonalizado,
+            @JsonProperty("cierre") String tipoCierre,
+            @JsonProperty("capacidad") int capacidad
+    ) {
         super(estilo, String.valueOf(esPersonalizado));
         this.tipoCierre = tipoCierre;
         this.capacidad = capacidad;
     }
-
     public Bolso(int codArt, String nombre, BigDecimal precio, String marca, String descripcion, String imagen, boolean activo, String estilo, boolean personalizado, String tipoCierre, int capacidad) {
     }
 
