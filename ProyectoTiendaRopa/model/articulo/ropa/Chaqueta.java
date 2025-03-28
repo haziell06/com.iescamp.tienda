275e5fa4de76db797a02a5ceb7878a4c70f51302
Christian <chrisgs2020@gmail.com>
2025-03-28 13:27:51 +0100

    Update Chaqueta.java

--- ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
+++ ProyectoTiendaRopa/model/articulo/ropa/Chaqueta.java
@@ -6,11 +6,6 @@ import java.math.BigDecimal;
 public class Chaqueta extends Ropa {
     private boolean impermeable;
 
-    public Chaqueta(String talla, String color, BigDecimal precio, String tipoCierre, String descripcion, String imagen, String s, String estilo, boolean impermeable, String string, String cierre, boolean b) {
-        super(talla, tipoCierre);
-        this.impermeable = impermeable;
-    }
-
     public Chaqueta(int codArt, String nombre, BigDecimal precio, String descripcion, String imagen, boolean activo, String marca, String color, Material material, String talla, String tipoCierre, boolean impermeable) {
         super(talla, tipoCierre);
         this.impermeable = impermeable;
